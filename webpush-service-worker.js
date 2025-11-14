ab84c9754ec6177f22d58d1c89b9b96bcb3b29e9
Urubandi Venkata krishna <venkatakrishnaurubandi@gmail.com>
2025-11-14 17:56:37 +0530

    Add files via upload

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BLhylbWUeJ02j6c2F2m-cceSfPtXMkDj4w8iEm4ZHD-1rK-QjJjIn_YP5LgoeiRZKaNRtiQFdEaTdKE6QuTxRwI=","apiKey":"ABEXvFYmNPe2WSKljaqtZqBHU","accountToken":"ABElKaqpK-3l_zDeOp-XArlkg","appver":"0.0.0","apiHost":"https://abr16c0-webpush.oraclersys.com","lazy":false}';
+orawp.init(config);
